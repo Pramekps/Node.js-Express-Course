bd5f3476ef99edca37a9a3a94e8f8380e49bcf99
Premkps <ppm.utcha@gmail.com>
2023-01-17 22:38:33 +0700

    Add all files of Project
    Hello :100:

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan')
+const app   = express();
+const port  = 3000;
+app.use(morgan('combined'));
+app.get("/", (req,res) => {
+    res.send('Hello Prem');
+})
+app.listen(port, ()=> {
+    debug("Listening on port %d",port);
+})
