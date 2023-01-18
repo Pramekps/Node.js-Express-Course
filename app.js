1764087596b63706f03d9a4f86a708b0919a1efe
Premkps <ppm.utcha@gmail.com>
2023-01-18 20:10:39 +0700

    dd

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const morgan = require('morgan')
 const path = require('path')
 
 const app   = express();
-const port  = 3000;
+const PORT  = process.env.PORT;
+
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")))
 
