4e8af22409301f870da340625fd13cce36a53b5b
Pramkps <ppm.utcha@gmail.com>
2023-01-22 21:27:42 +0700

    Route

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const debug = require('debug')('app');
-const morgan = require('morgan')
-const path = require('path')
+const morgan = require('morgan');
+const path = require('path');
+const chalk = require('chalk');
+const productRouter = express.Router();
 
-const app   = express();
-const PORT  = process.env.PORT || 4000;
+const app = express();
+const PORT = process.env.PORT || 4000;
+
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")))
-app.set("views","./src/views");
-app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname, "/public/")))
+app.set("views", "./src/views");
+app.set("view engine", "ejs")
 
-app.get("/", (req,res) => {
+productRouter.route("/").get((req, res) => {
+    res.send("Hello World !! I am products");
+});
+
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello World !! I am products1");
+});
+
+app.use("/products", productRouter)
+
+app.get("/", (req, res) => {
     //res.send('index');
-    res.render('index',{username: 'Prameza', customers : ['Prem','Utcha']});
+    res.render('index', { username: 'Prameza', customers: ['Prem', 'Utcha'] });
 })
-app.listen(PORT, ()=> {
-    debug("Listening on PORT %d",PORT);
+app.listen(PORT, () => {
+    debug("Listening on PORT : " + chalk.red(PORT));
 })
