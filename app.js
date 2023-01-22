0b5cb2e676e8640e07c07a055570a9c5a49f7270
Pramkps <ppm.utcha@gmail.com>
2023-01-22 22:46:26 +0700

    productRouter

--- app.js
+++ app.js
@@ -3,27 +3,17 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const chalk = require('chalk');
-const productRouter = express.Router();
-const products = require("./data/products.json")
+
 const app = express();
 const PORT = process.env.PORT || 4000;
+const productsRouter = require("./src/router/productsRouter")
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")))
 app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
-productRouter.route("/").get((req, res) => {
-    res.render("products",
-        products,
-    );
-});
-
-productRouter.route("/1").get((req, res) => {
-    res.send("Hello World !! I am products1");
-});
-
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 app.get("/", (req, res) => {
     //res.send('index');
